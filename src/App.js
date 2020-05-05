4d1520e5ffe7137d25573d9930c9f420cbc344ed
Milind Shyam <milindshyam@MacBook-Air-de-Milind.local>
2020-05-05 12:17:14 -0500

    moved html and css to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MilindPic from "./images/milind.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section id="container">
+      <h1>Hi, I am Milind.</h1>
+      <img src={MilindPic}></img>
+      <p>I am a software engineer who has worked on backend technologies and am now exploring the frontend.</p>
+      <button>LinkedIn</button>
+    </section>
   );
 }
 
